a805c3d22cb1d954e40c5c6af70084d438c14305
RyanLoughran13 <ryanaloughran@gmail.com>
2024-12-15 21:01:29 -0800

    Update thermal_control.py

--- TCS/thermal_control.py
+++ TCS/thermal_control.py
@@ -9,28 +9,17 @@ def process_temperature(input_temp, target_temp):
     applies a function to move it towards equilibrium.
     It should return a single float as the output indicating 
     the new, altered temperature"""
-
-############ MY CODE #########################################################
-    delta_t = target_temp-input_temp   #4.3.2 Finds difference         s #
-    print(f"Your total desired change in temperature is {delta_t}")          #
-    change_t = delta_t * 0.25   #4.3.4 How much my TCS is changing t by      #
-    print(f"25% of change in temperature for this iteration is {change_t}")  #
-    new_t = input_temp + change_t   #4.3.3 Changes t by 25%                  #
-    return new_t   #return new t                                     #
-                                                                             #
-input_temp = float(10)   #4.3.1 current temp                                #
-#target_temp = float(20)     #4.3.1 desired temp                             #
-print(f"Your current temperature for this iteration is {input_temp}")       #
-#print(f"Your target temperature for this iteration is {target_temp}")       #
-                                                                             #
-#new_temper = process_temperature(input_temp, target_temp)  #Calling function      #
-print(f"Your new current temperature is {new_t}")                            #
-##############################################################################
-
-
+    delta_t = target_temp-input_temp   #4.3.2 Finds difference         
+    print(f"Your total desired change in temperature is {delta_t:.2f}")          
+    change_t = delta_t * 0.25   #4.3.4 How much my TCS is changing t by      
+    print(f"25% of change in temperature for this iteration is {change_t:.2f}")  
+    new_t = input_temp + change_t   #4.3.3 Changes t by 25%                  
+    print(f"Your new current temperature is {new_t:.2f}")    
+    return new_t   #return new t                                     
+                                                                             
 ################### DO NOT EDIT THIS PORTION ############################
 def main():                                                             #
-    duration = 15      # Duration to feed signals in seconds            #                              #
+    duration = 15      # Duration to feed signals in seconds            #                              
     sequence = []                                                       #
     i = 0                                                               #
     temperature = random.randint(-30, 60)                               #
